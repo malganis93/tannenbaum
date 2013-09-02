a66685bcda243d11e39b4ef0fafcdd7f1ae4039c
Lars <Lars@192.168.1.110>
2013-09-02 10:17:26 +0200

    rechts geht

--- TannenbaumEA/Tannenbaum.java
+++ TannenbaumEA/Tannenbaum.java
@@ -3,13 +3,15 @@ public class Tannenbaum {
 
 	public static void main (String[] args)
 	{
-		int tag = 5;
+		int tag = 10;
 		System.out.println("TEST");
-		UP_Spitze(5);
-		for (int i=1;i<=tag;i++)
+		UP_Spitze(tag);
+		for (int i=1;i<tag;i++)
 		{
-			
+			UP_Links(tag,i);
+			UP_Rechts(tag,i);
 		}
+		UP_Stamm(tag);
 	}
 	
 	public static void UP_Spitze(int tag)
@@ -21,22 +23,37 @@ public class Tannenbaum {
 		System.out.println(sp+"ii");
 		
 	}
-	
-	public static void UP_links(int tag, int znr)
+	public static void UP_Stamm(int tag)
 	{
 		String sp = "";
-		for (int i =0; i<(tag-znr);i++)
-		{
-			sp+=".";
+		for (int i=0;i<tag*2-1;i++){
+			sp+="-";
 		}
-		System.out.println(sp+"i/");
+		
+		System.out.println(sp);
 		
 	}
 	
-	public static void UP_rechts(int tag, int znr)
+	public static void UP_Links(int tag, int znr)
 	{
+		String sp = "";
+		for (int i =0; i<(tag-znr-1);i++)
+		{
+			sp+=".";
+		}
+		System.out.print(sp+"i/");
 		
-		
+	}
+	
+	public static void UP_Rechts(int tag, int znr)
+	{
+		String sp = "";
+		for (int i=1; i<znr;i++)
+		{
+				sp+="..";
+			
+		}
+		System.out.println(sp+"\\i");
 	}
 	
 	
